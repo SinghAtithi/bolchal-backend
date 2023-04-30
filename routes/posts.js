3d9cf422de6e4ad1887430ab2918e3fa203930f9
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2023-04-30 16:28:39 +0530

    first commit

--- routes/posts.js
+++ routes/posts.js
@@ -5,23 +5,45 @@ const mongoose = require("mongoose");
 const postRouter = express.Router();
 
 const Posts = require("../models/posts");
+const Post = require("../models/posts");
 // const Post = require("../models/post.model");
 
 
 postRouter.get("/getAllPosts", (req, res) => {
       Posts.find({ verified: true })
+            .sort({ date: "desc" })
+            .then((posts) => res.json(posts))
+            .catch((err) => res.status(400).json("Error: " + err));
+});
+
+
+postRouter.get("/getUnverifiedPosts", (req, res) => {
+      console.log("Getting Unverified Posts");
+      Posts.find({ verified: false })
             .then((posts) => res.json(posts))
             .catch((err) => res.status(400).json("Error: " + err));
 })
 
+postRouter.post("/removePost", (req, res) => {
+      console.log(req.body);
+      Post.findOneAndDelete({ _id: req.body.id }).exec();
+      res.send("Post Removed");
+})
+
+postRouter.post("/verifyPost", (req, res) => {
+      Post.findOneAndUpdate({ _id: req.body.id }, { verified: true }).exec();
+      res.send("Post Verified");
+})
+
+
 postRouter.post("/write", (req, res) => {
-      console.log("I was here")
       const text = req.body.text;
+      console.log("Text : ", text);
       const newPost = new Posts({
             content: text,
-            userName: "Ravan",
+            userName: req.body.userName || "Annonymous",
             date: Date.now(),
-            verified: true
+            verified: false
       });
       newPost.save()
             .then(() => res.json("Post added!"))
